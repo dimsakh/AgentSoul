fccb0725845b4d4974ac7ac125f8345262451eef
dimsakh <124260944+dimsakh@users.noreply.github.com>
2026-07-28 14:50:28 +0300

    Export knowledge model

--- agentsoul_core/__init__.py
+++ agentsoul_core/__init__.py
@@ -1,7 +1,8 @@
 """Provider-neutral AgentSoul runtime core."""
 
 from .events import AgentEvent
+from .knowledge import KnowledgeItem, KnowledgeStore
 from .store import MemoryStore
 
-__all__ = ["AgentEvent", "MemoryStore"]
+__all__ = ["AgentEvent", "KnowledgeItem", "KnowledgeStore", "MemoryStore"]
 __version__ = "0.1.0"
